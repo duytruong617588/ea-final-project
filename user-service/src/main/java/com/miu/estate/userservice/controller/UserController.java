a4ad12ae789d517ed59e403797cae3d1160c2644
Dinh Trung <trantrung140219@gmail.com>
2024-04-20 15:51:40 -0500

    add verification api

--- user-service/src/main/java/com/miu/estate/userservice/controller/UserController.java
+++ user-service/src/main/java/com/miu/estate/userservice/controller/UserController.java
@@ -1,11 +1,18 @@
 package com.miu.estate.userservice.controller;
 
+import com.miu.estate.userservice.dto.response.UserDetailResponse;
+import com.miu.estate.userservice.model.User;
 import com.miu.estate.userservice.service.UserService;
+import com.miu.estate.userservice.utils.RequestUtil;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,6 +23,12 @@ public class UserController {
     @GetMapping("/list")
     public ResponseEntity<?> getAll() {
         return ResponseEntity.ok().body(userService.getAll());
+    }
+
+    @GetMapping("/verification")
+    public ResponseEntity<?> getUserById(HttpServletRequest request) {
+        User userLogin = RequestUtil.getUserLogin(request).orElseThrow(() -> new RuntimeException("User not found!"));
+        return ResponseEntity.ok().body(UserDetailResponse.fromUserEntity(userLogin));
     }
 
     @GetMapping("/test")
