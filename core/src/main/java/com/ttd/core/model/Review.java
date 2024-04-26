7b701db9b96b4e4dd2fd526323fbfda0c6c20e91
duy.truong <si.truong@miu.edu>
2024-04-25 23:40:30 -0500

    [Fix] Fix bug receive Review object from client

--- core/src/main/java/com/ttd/core/model/Review.java
+++ core/src/main/java/com/ttd/core/model/Review.java
@@ -1,4 +1,19 @@
 package com.ttd.core.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Review {
+	private Long id;
+	private String comment;
+	private int rating;
+	private Boolean isApproved = false;
+	private Long userId;
+	private Long propertyId;
+	private String createdDate;
+	private String updatedDate;
+	private boolean active;
+	private String deletedDate;
 }
