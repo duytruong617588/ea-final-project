d88e3e3c67398e5e511f30c16802ecdd27372d61
minhtoannguyenit <toanminh.nguyenit@gmail.com>
2024-04-25 23:31:36 -0500

    ADD data into mysql

--- property-service/src/main/java/com/miu/estate/repository/ReviewRepository.java
+++ property-service/src/main/java/com/miu/estate/repository/ReviewRepository.java
@@ -3,5 +3,8 @@ package com.miu.estate.repository;
 import com.miu.estate.model.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+    List<Review> findByPropertyId(Long id);
 }
